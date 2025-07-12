1944e6bf1efc42d06d7b2a4eaaa4013c606f4d62
nikitamuravitski <74623553+nikitamuravitski@users.noreply.github.com>
2025-07-12 13:32:27 +0300

    Update statusbar.rb

--- Formula/statusbar.rb
+++ Formula/statusbar.rb
@@ -2,7 +2,7 @@ class Statusbar < Formula
   desc "Minimal macOS status bar app with CLI"
   homepage "https://github.com/nikitamuravitski/homebrew-statusbar"
   url "https://github.com/nikitamuravitski/homebrew-statusbar/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "<SHA256_OF_TARBALL>"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on :macos
