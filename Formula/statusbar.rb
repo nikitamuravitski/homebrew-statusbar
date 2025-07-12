a123f34b75e527e1f1f4ea8d228ad115717e37f2
Mykyta Muravytskyi <nikita.muravitski@24slides.com>
2025-07-12 19:16:15 +0300

    Bump statusbar to v1.0.11

--- Formula/statusbar.rb
+++ Formula/statusbar.rb
@@ -1,8 +1,8 @@
 class Statusbar < Formula
   desc "Minimal macOS status bar app with CLI"
   homepage "https://github.com/nikitamuravitski/homebrew-statusbar"
-  url "https://github.com/nikitamuravitski/status-bar-extention/archive/refs/tags/v1.0.10.tar.gz"
-  sha256 "69c1b656385cef22fbc2e1cb49258c5905fffffec561b9321c262d24ee619611"
+  url "https://github.com/nikitamuravitski/status-bar-extention/archive/refs/tags/v1.0.11.tar.gz"
+  sha256 "42799f2ce2088b700481d86eea496b3f95909402822d888ba3c20a46e5d1a82b"
   license "MIT"
 
   depends_on :macos
