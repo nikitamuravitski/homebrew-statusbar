a9f362f729ccaf67e8e6617a74e626261c5a906f
Mykyta Muravytskyi <nikita.muravitski@24slides.com>
2025-07-12 13:29:54 +0300

    Add statusbar formula

---
+++ Formula/statusbar.rb
@@ -0,0 +1,20 @@
+class Statusbar < Formula
+  desc "Minimal macOS status bar app with CLI"
+  homepage "https://github.com/nikitamuravitski/homebrew-statusbar"
+  url "https://github.com/nikitamuravitski/homebrew-statusbar/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "<SHA256_OF_TARBALL>"
+  license "MIT"
+
+  depends_on :macos
+  depends_on "swift" => :build
+
+  def install
+    system "./build.sh"
+    bin.install "bin/statusbar"
+    bin.install "bin/statusbar-bin"
+  end
+
+  test do
+    system "#{bin}/statusbar", "--help"
+  end
+end
